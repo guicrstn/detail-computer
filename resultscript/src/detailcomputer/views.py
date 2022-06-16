5fb1d82f67c84b804465a6860b07cb76954fc5ac
guicrstn <gcristini@app.afpma.fr>
2022-06-16 16:17:45 +0200

    add blog

--- resultscript/src/detailcomputer/views.py
+++ resultscript/src/detailcomputer/views.py
@@ -2,7 +2,10 @@
 
 from django.http import HttpResponse
 from django.http import HttpResponse
+from datetime import datetime
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html', context={"prenom": "guillaume"})
+
+    return render(request, 'index.html', context={"date": datetime.today()})
